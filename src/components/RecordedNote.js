412595559ec0fb8ce765d03903ca996c7242b8b6
Zahra Fakoor <za.fakoor@gmail.com>
2022-03-07 11:39:21 +0100

    handled multi language

--- src/components/RecordedNote.js
+++ src/components/RecordedNote.js
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { Button, Card, Grid, Typography } from "@material-ui/core"
+import { Box, Button, Card, FormControl, Grid, InputLabel, MenuItem, Select, Typography } from "@material-ui/core"
 import { useState, useEffect } from "react"
 import { withStyles } from "@material-ui/core/styles";
 
@@ -9,37 +9,56 @@ const mic = new SpeechRecognition()
 
 mic.continuous = true
 mic.interimResults = true
-mic.lang = 'en-US'
 
 const styles = (theme) => ({
-noteContainer: {
-    height: "100%",
-    maxHeight: 100,
-    padding:5
-}
+    noteContainer: {
+        height: "100%",
+        maxHeight: 100,
+        padding: 5
+    },
+    langSettingContainer: {
+        display: 'flex',
+        justifyContent: 'space-between'
+    }
 });
 
 const RecordedNote = ({ capturing, classes }) => {
     const [note, setNote] = useState(null)
     const [savedNotes, setSavedNotes] = useState([])
+    const [lang, setLang] = useState('en-US')
+
+    const handleLangChange = (e) => {
+        setLang(e.target.value)
+    }
 
     const handleListen = () => {
+        mic.lang = lang;
         if (capturing) {
             mic.start()
             mic.onend = () => {
+                console.log('continue..')
                 mic.start()
             }
         } else {
             mic.stop()
+            mic.onend = () => {
+                console.log('Stopped Mic on Click')
+            }
         }
-
+        mic.onstart = () => {
+            console.log('Mics on')
+        }
 
         mic.onresult = event => {
             const transcript = Array.from(event.results)
                 .map(result => result[0])
                 .map(result => result.transcript)
                 .join('')
+            console.log(transcript)
             setNote(transcript)
+            mic.onerror = event => {
+                console.log(event.error)
+            }
         }
     }
     const handleSaveNote = () => {
@@ -56,20 +75,38 @@ const RecordedNote = ({ capturing, classes }) => {
             <Grid container spacing={2}>
                 <Grid item xs={6}>
                     <Card className={classes.noteContainer}>
-                    <Typography variant="body2">
-                        {note ? note : "Say something ..."}
-                    </Typography>
-                    <Button onClick={handleSaveNote} disabled={!note || capturing}>
-                        Save Note
-                    </Button>
+                        <Box className={classes.langSettingContainer}>
+                            <FormControl fullWidth>
+                                <InputLabel id="demo-simple-select-label">Language</InputLabel>
+                                <Select
+                                    labelId="demo-simple-select-label"
+                                    id="demo-simple-select"
+                                    value={lang}
+                                    label="Language"
+                                    onChange={handleLangChange}
+                                    disabled={capturing}
+                                >
+                                    <MenuItem value={'en-US'}>English</MenuItem>
+                                    <MenuItem value={'de-DE'}>Deutsch</MenuItem>
+                                    <MenuItem value={'ar-SA'}>Arabic</MenuItem>
+                                </Select>
+                            </FormControl>
+                            <Button onClick={handleSaveNote} disabled={!note || capturing}>
+                                Save Note
+                            </Button>
+                        </Box>
+
+                        <Typography variant="body2">
+                            {note ? note : "Say something ..."}
+                        </Typography>
                     </Card>
                 </Grid>
                 <Grid item xs={6}>
                     <Card className={classes.noteContainer}>
-                    <Typography variant="h6">Notes</Typography>
-                    {savedNotes.map((n,index) => (
-                        <Typography key={index}>{n}</Typography>
-                    ))}
+                        <Typography variant="h6">Notes</Typography>
+                        {savedNotes.map((n) => (
+                            <Typography key={n}>{n}</Typography>
+                        ))}
                     </Card>
                 </Grid>
             </Grid>
